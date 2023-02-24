ab9c85d0e85ba1c74305a3df7e70ec0ea5c7d0b6
Matheus <matheusdsr@hotmail.com>
2023-02-24 15:29:22 -0300

    Adicionado interface grafica, com PySimpleGUI

--- password_generator.py
+++ password_generator.py
@@ -1,25 +1,46 @@
 import random
 import string
+import PySimpleGUI as sg
 
-def password_generator(Len_pass = 8):
-    ascii_options = string.ascii_letters
-    number_option = string.digits
-    punt_options = string.punctuation
-    option = ascii_options + number_option + punt_options
-    password_user = ""
-    for i in range(0, Len_pass):
-        digit = random.choice(option)
-        password_user = password_user + digit
-    return password_user
-
-choice_user = input("Quantos digitos na senha?")
-    
-if choice_user.isdigit():
-    choice_user = int(choice_user)
-else:
-    print("Entrada invalida!")
-    quit()
+class PassGen:
+    def __init__(self):
+        sg.theme('Black')
+        layout = [
+            [sg.Text('Site/Software', size=(10,1)),
+            sg.Input(key='site',size=(20,1))],
+            [sg.Text('E-mail/Usuario', size=(10,1)),
+            sg.Input(key='usuario',size=(20,1))],
+            [sg.Text('Quantidade de caracteres', size=(10,1)), sg.Combo(values=list(range(30)),key='total_chars', default_value=1,size=(3,1))],
+            [sg.Output(size=(32,5))],
+            [sg.Button('Gerar senha')]
+        ]
+        self.janela = sg.Window('Password Generator', layout)
         
-response = password_generator(Len_pass = choice_user)
+    def Iniciar(self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+            if evento == 'Gerar senha':
+                nova_senha = self.password_generator(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha, valores)
+                
+    def salvar_senha(self, nova_senha, valores):
+        with open('./senha.txt', 'a', newline='') as arquivo:
+            arquivo.write(f"site:{valores['site']}, usuario:{valores['usuario']}, nova senha: {nova_senha}\n")
+        print('Senha salva para o arquivo "Senhas.txt"')
 
-print(f"Senha gerada:\n{response}")
+    def password_generator(self, Len_pass):
+        ascii_options = string.ascii_letters
+        number_option = string.digits
+        punt_options = string.punctuation
+        option = ascii_options + number_option + punt_options
+        password_user = ""
+        for i in range(0, Len_pass['total_chars']):
+            digit = random.choice(option)
+            password_user = password_user + digit
+        return password_user
+
+gen = PassGen()
+gen.Iniciar()
